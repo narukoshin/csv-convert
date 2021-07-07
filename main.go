bc7a6b9308e02c52cbc17eb5b3a9568881c28765
yuuhirokabe <alekss273@inbox.lv>
2021-07-07 16:27:08 +0300

    Added necessary packages for this software

---
+++ main.go
@@ -0,0 +1,20 @@
+/***
+
+	This app is sponsored by business @ xparts.lv 2021
+		- Code written by Naru Koshin (Github: https://github.com/narukoshin)
+		- This code is open source and available at https://github.com/narukoshin/csv-convert
+
+***/
+
+
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+func main(){
+
+}
